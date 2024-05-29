cbb4d1d98dbbdd20e91ba3e15a64fe81a8a04fbb
Tobias Hahnen <Tobias.hahnen@sonarsource.com>
2024-05-29 16:42:25 +0200

    Changed first script to correct debugging structure
    The other scripts need to be changed too, the stuff in "runConfiguration" can be removed in favor of attaching to process.
    Documentation needs to be changed.

--- debug/idz-16.0.4.sh
+++ debug/idz-16.0.4.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-export SONARLINT_JVM_OPTS="-agentlib:jdwp=transport=dt_socket,server=n,address=localhost:9009,suspend=y"
+export SONARLINT_JVM_OPTS="-agentlib:jdwp=transport=dt_socket,server=y,address=localhost:9009,suspend=y"
 /opt/idz-16.0.4/developer_for_zos
